af6c0f4f35f7dcfb1d4b7897119daaea47f5b40e
semi-infiknight <mathurshubhang2002@gmail.com>
2024-03-23 08:56:52 +0530

    fix

--- frontend-randomplay/src/components/Home/Home.tsx
+++ frontend-randomplay/src/components/Home/Home.tsx
@@ -9,7 +9,7 @@ export default function Home() {
                         Experience True On-Chain
                     </h1>
                     <h1 className="!m-0 font-bold text-[#CEB7B7]">
-                        Multiplayer Games
+                     Games
                     </h1>
                     <p className="text-[#8FB1B9] w-8/12 text-xl">
                         Randomplay Utilizes Aptos Randomness API to Power our map generator to make truly on chain random maps for games not needing to rely on External external Web2 Technologies for making Maps</p>
